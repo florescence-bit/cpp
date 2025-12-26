0e33c5e0c074cb820069abe84f8105f41da27e74
san <adityarajsharma.021@gamil.com>
2025-12-26 15:21:16 +0530

    A normal mask

--- iod.cpp
+++ iod.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 int main () {
     canvas can1;
+    cout << "\033[2J";
     can1.rect(3,3,3,3);
     can1.rect(14,3,3,3);
+    can1.rect(9,9,2,2);
     can1.rect(3,14,14,3);
     can1.draw();
     return 0;
