6c6cfb1723daefe73b5e37b4d95c66427b587513
san <adityarajsharma.021@gamil.com>
2025-12-26 12:33:10 +0530

    frame done

--- canvas.h
+++ canvas.h
@@ -5,16 +5,19 @@ using namespace std;
 
 class canvas {
     public:
-    char fill = 'x';
+    char fill = 'm';
     char empt = ' ';
     int width = 20;
     int height = 20;
+    int frame [20][20];
     public:
     canvas () {
-        cout << fill << empt << endl;
-        cout << "canvas rendered" << endl;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++)
+	        cout << fill << fill;
+	    cout << endl;
+	}
     }
-    
 };
 
 #endif
--- iod.cpp
+++ iod.cpp
@@ -2,9 +2,6 @@
 #include "canvas.h"
 using namespace std;
 int main () {
-    cout << "working..." << endl;
     canvas can1;
-    cout << can1.width << endl;
-    cout << can1.height << endl;
     return 0;
 }
