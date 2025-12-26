f141d58aa54c37a7eb162aa90d0bc82951c23485
san <adityarajsharma.021@gamil.com>
2025-12-26 14:44:40 +0530

    mask

--- canvas.h
+++ canvas.h
@@ -5,17 +5,41 @@ using namespace std;
 
 class canvas {
     public:
-    char fill = 'm';
-    char empt = ' ';
     int width = 20;
     int height = 20;
     int frame [20][20];
     public:
     canvas () {
+	fill();	
+    }
+    public:
+    void clear () {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)
-	        cout << fill << fill;
+		frame[y][x] = 0;
+	}
+    }
+    void fill () {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++)
+		frame[y][x] = 1;
+	}
+    }
+    public:
+    void draw () {
+	for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++)
+		if(frame[y][x])
+	        cout << "MM";
+	        else cout << "  ";
 	    cout << endl;
+	}
+    }
+    public:
+    void rect (int X, int Y, int W, int H) {
+        for(int y = Y; y < Y + H; y++) {
+	    for(int x = X; x < X + W; x++)
+		frame[y][x] = 0;
 	}
     }
 };
--- iod.cpp
+++ iod.cpp
@@ -3,5 +3,9 @@
 using namespace std;
 int main () {
     canvas can1;
+    can1.rect(3,3,3,3);
+    can1.rect(14,3,3,3);
+    can1.rect(3,14,14,3);
+    can1.draw();
     return 0;
 }
