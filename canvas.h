f7f59ac9eb4b9aae039cf3cd5bffd1e98cba21b9
san <adityarajsharma.021@gamil.com>
2025-12-26 11:38:13 +0530

    custom header

---
+++ canvas.h
@@ -0,0 +1,10 @@
+#ifndef canvas_h
+#define canvas_h
+
+int iamc();
+
+int iamc() {
+    return 1;
+}
+
+#endif
--- iod.cpp
+++ iod.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "canvas.h"
 using namespace std;
 int main () {
     cout << "working..." << endl;
+    extern int x;
+    cout << iamc() << endl;
     return 0;
 }
